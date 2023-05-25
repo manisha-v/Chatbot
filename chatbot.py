5aeea4459b4746258a11f9fc5134e86d9b7e423e
Manisha Varshney <94631536+manisha-v@users.noreply.github.com>
2023-05-25 23:33:15 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,21 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "OPENAI_API_KEY"
 
 chat_history = [
-    {"role": "system", "content": "Hello user! How may i help?"},
+    {"role": "system", "content": "Hello user! How may I help?"},
     {"role": "user", "content": "Which team won the IPL 2018"}
 ]
+
+for mes in chat_history:
+    print(mes["role"],": ",mes["content"])
 
 while(True):
     response = openai.ChatCompletion.create( model="gpt-3.5-turbo", messages=chat_history )
     reply = response.choices[0].message.content
-    print("System: ", reply)
+    print("system: ", reply)
     user_mes = input("user: ")
-    if(user_mes == "done"):
+    if(user_mes == "bye"): # ends when user says bye
         break
     user_message = {"role":"user", "content":user_mes}
     chat_history.append(user_message)
-
-
-
